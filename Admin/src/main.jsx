3f9c22ec435413570af9b47f979dbf2028b2e5df
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-08-27 22:43:26 +0530

    main.jsx updated

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -8,12 +7,10 @@ import store from './redux/store.js'
 import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
         <Toaster />
       </Provider>
     </BrowserRouter>
-  </StrictMode>,
 )
