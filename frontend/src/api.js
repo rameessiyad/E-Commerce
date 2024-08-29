3f69d78fdaa031d48793c8add636867458b96501
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-08-29 18:22:22 +0530

    frontend updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,2 @@
-// export const BaseURL = 'https://e-commerce-ab3p.onrender.com/api/v1';
-export const BaseURL = 'http://localhost:3000/api/v1';
+export const BaseURL = 'https://e-commerce-ab3p.onrender.com/api/v1';
+// export const BaseURL = 'http://localhost:3000/api/v1';
