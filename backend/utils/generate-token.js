c55a306c377a011e75dc8f934a38302857969713
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-08-29 08:10:47 +0530

    cookie configuration updated

--- backend/utils/generate-token.js
+++ backend/utils/generate-token.js
@@ -4,7 +4,7 @@ const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1d' });
     res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== 'development',
+        secure: process.env.NODE_ENV !== 'production',
         sameSite: 'None',
         maxAge: 1000 * 60 * 60 * 24
     })
