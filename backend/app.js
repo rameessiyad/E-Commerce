4d3bab3f927ab29fbe931c4c77d0719549bb4347
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-08-29 18:37:59 +0530

    cors configuration updated

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,12 @@ const { notFound, errorHandler } = require('./middlewares/error-middleware');
 const app = express();
 
 // Allowed origins
-const allowedOrigins = ['http://localhost:5000', 'https://e-commerce-chi-ochre.vercel.app', 'https://mens-cart-admin.vercel.app', 'http://localhost:4000'];
+const allowedOrigins = [
+    'http://localhost:5000',
+    'https://e-commerce-chi-ochre.vercel.app',
+    'https://mens-cart-admin.vercel.app',
+    'http://localhost:4000'
+];
 
 //middlewares
 app.use(express.json());
